d41efa3e1389f7ba5521483ef18f1b7e926447ff
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-14 00:49:44 +0200

    start of homepage finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
+import { Rowing } from "@mui/icons-material";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack
+        style={{
+          flexDirection: "row",
+          gap: "40px",
+          fontSize: "24px",
+          alignItems: "flex-end",
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3a1212",
+            borderBottom: "3px solid #ff2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3a1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 };
